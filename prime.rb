19decd9996923aae0c1a6b94fcce1c077f0f5a29
Ricartho Franck <ricartho.franck@esih.edu>
2019-04-16 14:33:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,14 @@
 # Add  code here!
 def prime?(val)
   val_range = (2...val)
+  i = 2 
   if val <= 1 
     false 
   end 
   val_range.each do |digit|
-    
+    if digit % 2 == 0 
+      true 
+    end 
+    return false 
   end 
 end 
